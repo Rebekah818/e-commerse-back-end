3508aea9be93bc5d66e0d6c74936eddb478c1302
Rebekah 2021 <rebekahmcdevitt@gmail.com>
2022-03-10 19:28:01 -0500

    added columns to the models

--- models/Product.js
+++ models/Product.js
@@ -3,14 +3,48 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { DataTypes } = require('sequelize/types');
 
-// Products belongsTo Category
+//product model
+//define the columns
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNULL: false,
+      primarykey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
+  },
+  
+)
 
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
 
 module.exports = {
   Product,
--- models/Tag.js
+++ models/Tag.js
@@ -1,4 +1,4 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INTEGER } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
@@ -7,6 +7,20 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    },
+    
   },
   {
     sequelize,
