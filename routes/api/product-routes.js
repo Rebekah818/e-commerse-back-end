cf988a2f57fe77975bb60b73d9e89cf30d7f8933
Rebekah 2021 <rebekahmcdevitt@gmail.com>
2022-03-11 09:07:30 -0500

    added password, username, db name to .env

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,28 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({ include: ({ model: Category }, { model: Tag }) });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findById(req.params.id, { include: ({ model: Category }, { model: Tag }) });
+    if (!productData) {
+      res.status(404).json({ message: "No product with this id" })
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -87,10 +103,25 @@ router.put('/:id', (req, res) => {
       // console.log(err);
       res.status(400).json(err);
     });
-});
+})
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
-});
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'message: no product found with this id!' });
+      return;
+    }
+    
+    res.status(200).json(productData);
+    } catch (err) {
+    res.status(500).json(err)
+    }
+    });
 
 module.exports = router;
