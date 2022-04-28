810ad2dc5067cff53af257c979436de984a6b3fe
Rebekah 2021 <rebekahmcdevitt@gmail.com>
2022-04-28 11:05:28 -0400

    concatanated api routes in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
-const categoryRoutes = require('./api/categories');
-const productRoutes = require('./api/products');
-const tagRoutes = require('./api/tags');
+// const categoryRoutes = require('./api/categories');
+// // const productRoutes = require('./api/products');
+// // const tagRoutes = require('./api/tags');
+const apiRoutes = require('./api');
 
-router.use('./models/category', categoryRoutes);
-router.use('./models/product', productRoutes);
-router.use('./models/tag', tagRoutes);
+router.use('/api', apiRoutes);
+// router.use('./models/category', categoryRoutes);
+// router.use('./models/product', productRoutes);
+// router.use('./models/tag', tagRoutes);
 
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+  return res.send('Wrong Route!');
 });
 
 module.exports = router;
